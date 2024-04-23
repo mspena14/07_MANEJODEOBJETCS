611036cd5abc943bb9ea3c9ef8080b5d82bc9b03
mspena14 <marlono1naranjo@gmail.com>
2024-04-23 10:42:08 -0500

    Creamos objeto

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+const coder = {
+    id: "c001",
+    name: "marlon",
+    lastName: "peña",
+    birthDate: new Date("2000-12-23 18:00:00"),
+    indentificacionNumber: "1007405332",
+    email: "marlono1naranjo@gmail.com",
+    clan: "dell",
+    phoneNumber: "3194746457",
+    codebtor: "janeth naranjo",
+    address: "cr 50a # 58-08",
+    workingDay: "media",
+    saludar: function () { 
+        console.log("hola mundo")
+    }
+}
